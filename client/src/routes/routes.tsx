6e8c80a48840133528cf6501854e4878ccc4c5a2
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-27 23:34:45 +0530

    Added new routes for navigation

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,13 +1,13 @@
-import Dashboard from "@/pages/Dashbord"
 import LoginPage from "@/pages/LoginPage"
 import { BrowserRouter, Route, Routes } from "react-router"
+import Report from "@/pages/DailyReports"
 
 export const Router = () => {
     return <>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dailyReport" element={<Report />} />
             </Routes>
         </BrowserRouter>
     </>
