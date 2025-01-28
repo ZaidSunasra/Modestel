55202b892934a3adf065f5527fc127463ff01e99
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-28 23:15:52 +0530

    Added delete routes api and mutation

--- client/src/api/reportApi.ts
+++ client/src/api/reportApi.ts
@@ -4,3 +4,8 @@ export  const fetchReports = async() : Promise<any> => {
     const response = await axiosInstance.get("/report/get");
     return response.data;
 }
+
+export const deleteReport = async(id: string): Promise<any> => {
+    const response = await axiosInstance.delete(`/report/delete/${id}`);
+    return response.data;
+}
---
+++ client/src/mutations/reportMutation.ts
@@ -0,0 +1,36 @@
+import { deleteReport } from "@/api/reportApi"
+import { useToast } from "@/hooks/use-toast"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useNavigate } from "react-router";
+
+export const useDeleteReport = () => {
+
+    const {toast } = useToast();
+    const navigate =  useNavigate();
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: deleteReport,
+        onSuccess : (data: any) => {
+            toast({
+                variant: "default",
+                title: data.msg,
+                duration: 3000
+            });
+            queryClient.invalidateQueries({ queryKey: ['reports']});
+            queryClient.invalidateQueries({ queryKey: ['daily-cash-total']});
+            queryClient.invalidateQueries({ queryKey:  ['daily-income-payment-mode']});
+            queryClient.invalidateQueries({ queryKey: ['daily-income-booking-mode']});
+            queryClient.invalidateQueries({ queryKey: ['daily-income-source']});
+            navigate("/dailyReport");
+        },
+        onError : (error: any) => {
+            toast({
+                variant: "destructive",
+                title: error.response.data.msg,
+                duration: 3000
+            });
+            navigate("/dailyReport")
+        }
+    })
+}
